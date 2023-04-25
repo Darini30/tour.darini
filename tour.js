de73461cdb1ce5d07e7b3ddde4b4de94905fa082
Darini30 <124670888+Darini30@users.noreply.github.com>
2023-04-25 16:23:15 +0700

    Add files via upload

---
+++ tour.js
@@ -0,0 +1,156 @@
+var tourpoint = {
+    
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Atothaya",
+              "type": "special",
+              "level": 10
+            },
+            "geometry": {
+              "coordinates": [
+                100.59149072501077,
+                14.353649444364521
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "WatYaichaimongkol",
+              "type": "cultrue",
+              "level": 10
+            },
+            "geometry": {
+              "coordinates": [
+                100.59263559983191,
+                14.345737465505678
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "AyothayaFloatingMarket",
+              "type": "special",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                100.5932734325429,
+                14.359133897276408
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "WatChaiWatthanaram",
+              "type": "culture",
+              "level": 10
+            },
+            "geometry": {
+              "coordinates": [
+                100.54168349999725,
+                14.343306843693355
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "BangPaInRoyalPalace",
+              "type": "culture",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                100.57932880688605,
+                14.232238183980073
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "WatMahaThat",
+              "type": "culture",
+              "level": 10
+            },
+            "geometry": {
+              "coordinates": [
+                100.56756277909727,
+                14.356704784141812
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "ElephantKraal",
+              "type": "natural",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                100.56936399999648,
+                14.377875314429374
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "WatPhraSiSanphet",
+              "type": "culture",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                100.55861027116208,
+                14.35522234601703
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "KongKhongMarket",
+              "type": "special",
+              "level": 8
+            },
+            "geometry": {
+              "coordinates": [
+                100.57618599999796,
+                14.283113331992254
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "PrasatNakhonLuang",
+              "type": "cultural",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                100.61102000000136,
+                14.465807533752056
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+      }
